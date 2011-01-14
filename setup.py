f65d4d9aaa9801f37f1f250be11ee968d5bf9592
David Barnett <davidbarnett2@gmail.com>
2011-01-14 17:16:05 -0500

    Added basic setup.py with 'matplotlib>=1' dependency

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "genetic-programming",
+    version = "0.01",
+    packages=find_packages(),
+    dependency_links = [],
+    install_requires=['matplotlib>=1'],
+    extras_require={},
+    package_data = {},
+    author= "Timothy Barnett",
+    author_email = "",
+    description = "",
+    license = "",
+    keywords= "",
+    url = "http://github.com/sidious1741/python-genetic-programming",
+    entry_points = {
+        "console_scripts": []
+    }
+)
